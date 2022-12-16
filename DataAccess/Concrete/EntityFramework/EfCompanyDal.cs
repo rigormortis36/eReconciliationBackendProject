d9f2ea041c07dd3734b582726c26595a3bc71fa1
rigormortis36 <91247220+rigormortis36@users.noreply.github.com>
2022-12-16 14:10:57 +0300

    Add files via upload

--- DataAccess/Concrete/EntityFramework/EfCompanyDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCompanyDal.cs
@@ -1,9 +1,11 @@
 ﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Context;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,31 @@ using System.Windows.Input;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCompanyDal: EfEntityRepositoryBase<Company, ContextDb>, ICompanyDal
+    public class EfCompanyDal : EfEntityRepositoryBase<Company, ContextDb>, ICompanyDal
     {
+        public UserCompany GetCompany(int userId)
+        {
+            using (var context = new ContextDb())
+            {
+                var result = context.UserCompanies.Where(p => p.UserId == userId).FirstOrDefault();
+                return result;
+            }
+        }
+
+        public void UserCompanyAdd(int userId, int companyId)
+        {
+            using (var context = new ContextDb())
+            {
+                UserCompany userCompany = new UserCompany()
+                {
+                    UserId = userId,
+                    CompanyId = companyId,
+                    AddedAt = DateTime.Now,
+                    IsActive = true,
+                };
+                context.UserCompanies.Add(userCompany);
+                context.SaveChanges();
+            }
+        }
     }
 }
